40d6627e84f0d5ac261262e978f3175ea81ce2b1
Amircolgang <parham38385747@gmail.com>
2025-06-04 15:48:32 +0330

    esm

---
+++ ESM/funcs/founc.js
@@ -0,0 +1,7 @@
+let concat = (name , lname) => {
+    let b = name + lname
+    return b
+}
+
+
+export {concat}
---
+++ ESM/server.js
@@ -0,0 +1,3 @@
+import { concat } from "./funcs/founc.js";
+
+console.log(concat("Amir " , "Shirazi"))
