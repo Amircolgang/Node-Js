246b515eb1e02e02ba891de51834834fb389a8b7
Amircolgang <parham38385747@gmail.com>
2025-06-04 23:14:25 +0330

    http posts url

--- http/server.js
+++ http/server.js
@@ -1,14 +1,19 @@
 const http = require("node:http")
 
 
-const server = http.createServer((req , res)=>{
-    console.log(req.method)
-    console.log(req.url)
-    // console.log(res)
-    res.write("Welcom to Node Js Server")
-    res.end()
+const server = http.createServer((req, res) => {
+    if (req.method == "GET" && req.url == "/") {
+        res.write("Welcom To Home :)")
+        res.end()
+    } else if (req.method == "GET" && req.url == "/posts") {
+        res.write("Welcom To Posts :)")
+        res.end()
+    }else if (req.method == "GET" && req.url == "/users") {
+        res.write("Welcom To Users :)")
+        res.end()
+    }
 });
 
-server.listen(3000, ()=>{
+server.listen(3000, () => {
     console.log("Server Run In Port 3000")
 })
