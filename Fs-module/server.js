d44facc77a7800616ce1cdb5eef8460eb7184627
Amircolgang <parham38385747@gmail.com>
2025-06-05 15:17:30 +0330

    creat user.json

--- Fs-module/server.js
+++ Fs-module/server.js
@@ -1,3 +1,10 @@
 const fs = require('fs')
 
-fs.readFile('./name.txt',)
+// fs.readFile('./name.txt',(err , data )=>{
+//     if(err){
+//         throw err
+//     }
+//     console.log(
+//         `Your Readed File Is : \n ${data}`
+//     )
+// })
